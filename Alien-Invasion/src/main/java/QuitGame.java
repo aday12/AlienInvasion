0fa8194e9801f434a66ffcd59c17f84a2c5adf8f
Aaron Day <aaronday1996@icloud.com>
2024-01-26 13:42:26 -0800

    Text Parser, AB#157

--- Alien-Invasion/src/main/java/QuitGame.java
+++ Alien-Invasion/src/main/java/QuitGame.java
@@ -1,15 +1,27 @@
 import java.util.Scanner;
 
-class QuitGame {
+public class QuitGame {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Type [Q]uit at any time to end the game!");
 
-        String userInput = scan.nextLine().trim().toUpperCase();
+        String userInput = scan.nextLine().trim();
 
-        if (userInput.equals("Q")) {
+        System.out.println("Enter a verb or a verb + a noun to interact: ");
+
+
+        if (userInput.equalsIgnoreCase("Q")) {
             System.exit(0);
         }
+        else {
+            String[] cmd = userInput.split(" ");
+            System.out.println("Verb: " + cmd[0]);
+            System.out.println("Noun: " + cmd[1]);
+                if (cmd.length > 2) {
+                    System.out.println("Please only enter 1 verb or 1 verb and 1 noun");
+                }
+        }
+        scan.close();
     }
 }

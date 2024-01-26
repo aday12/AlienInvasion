b942d762bda79126952100545cf9fab820eaff34
Aaron Day <aaronday1996@icloud.com>
2024-01-26 12:50:30 -0800

    Players can quit game by typing Quit, AB#155

--- Alien-Invasion/src/main/java/QuitGame.java
+++ Alien-Invasion/src/main/java/QuitGame.java
@@ -1,3 +1,15 @@
-package PACKAGE_NAME;class QuitGame {
-    
+import java.util.Scanner;
+
+class QuitGame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Type [Q]uit at any time to end the game!");
+
+        String userInput = scan.nextLine().trim().toUpperCase();
+
+        if (userInput.equals("Q")) {
+            System.exit(0);
+        }
+    }
 }

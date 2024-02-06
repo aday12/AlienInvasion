77c239b36e372df42ac9103ba78a6cadcb85d7a4
aday12 <aaronday1996@icloud.com>
2024-02-06 14:29:31 -0800

    Quit method completed modified Controller class, AB#155

--- Alien-Invasion/src/main/java/AlienInvasionIntro.java
+++ Alien-Invasion/src/main/java/AlienInvasionIntro.java
@@ -4,22 +4,22 @@ public class AlienInvasionIntro {
 
     public static void welcome() throws InterruptedException {
 
-//        String story = "Welcome, Space Commander, to the thrilling adventure of 'Alien Invasion.' In this cosmic journey, you will assume the role of the fearless commander of Starship TLG Alactica, embarking on a mission that will challenge your wits and courage. As you decipher signals, navigate interstellar politics, and unveil ancient cosmic secrets, the destiny of humanity will rest in your hands. Are you ready to embark on this epic quest to shape the future of our world?";
-//
-//        String objective = "Your objective is to explore the the Starship uncovering hidden mysteries and puzzles.  You will be tasked to solve various problems, puzzles, and mysteries that will ultimately shape the destiny of humanity.";
-//
-//        String playerIdentity = "As the Space Commander of Starship TLG Alactica, you are humanity's last hope.";
-//
-//        String winGame = "In order to achieve victory in the game, you must skillfully tackle challenges, riddles, and clues that culminate in an ultimate confrontation between humanity and the impending alien invasion!";
-//
-//        clearConsole();
-//        printWithTypewriterEffect(story);
-//        clearConsolePause(1000);
-//        printWithTypewriterEffect(objective);
-//        clearConsolePause(1000);
-//        printWithTypewriterEffect(playerIdentity);
-//        clearConsolePause(1000);
-//        printWithTypewriterEffect(winGame);
+        String story = "Welcome, Space Commander, to the thrilling adventure of 'Alien Invasion.' In this cosmic journey, you will assume the role of the fearless commander of Starship TLG Alactica, embarking on a mission that will challenge your wits and courage. As you decipher signals, navigate interstellar politics, and unveil ancient cosmic secrets, the destiny of humanity will rest in your hands. Are you ready to embark on this epic quest to shape the future of our world?";
+
+        String objective = "Your objective is to explore the the Starship uncovering hidden mysteries and puzzles.  You will be tasked to solve various problems, puzzles, and mysteries that will ultimately shape the destiny of humanity.";
+
+        String playerIdentity = "As the Space Commander of Starship TLG Alactica, you are humanity's last hope.";
+
+        String winGame = "In order to achieve victory in the game, you must skillfully tackle challenges, riddles, and clues that culminate in an ultimate confrontation between humanity and the impending alien invasion!";
+
+        clearConsole();
+        printWithTypewriterEffect(story);
+        clearConsolePause(1000);
+        printWithTypewriterEffect(objective);
+        clearConsolePause(1000);
+        printWithTypewriterEffect(playerIdentity);
+        clearConsolePause(1000);
+        printWithTypewriterEffect(winGame);
         System.out.println("Press enter to continue");
     }
 
--- Alien-Invasion/src/main/java/Controller.java
+++ Alien-Invasion/src/main/java/Controller.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Controller {
     public static void main(String[] args) throws InterruptedException {
         Scanner scan = new Scanner(System.in);
-        TitleScreen.displayAsciiArt("Alien-Invasion/static/title.txt");
+        TitleScreen.displayAsciiArt("./static/title.txt");
 
         if (NewGame.gameStart()) {
-            AlienInvasionIntro.welcome();
+//            AlienInvasionIntro.welcome();
             boolean isGame = true;
             while (isGame) {
                // PlayerLocation.location();
+                System.out.println("What's your next move Commander?");
                 System.out.print("> ");
                 String userInput = scan.nextLine().trim();
                 TextParser.textParser(userInput);

1a0c08d8887275a1565bcd0c4e87bddaaed7cc3c
KNGLIE <lancee6@gmail.com>
2024-02-06 08:13:29 -0700

    converted itemsjson to use gson, AB#169

--- Alien-Invasion/src/main/java/ItemsJSON.java
+++ Alien-Invasion/src/main/java/ItemsJSON.java
@@ -1,36 +1,33 @@
-import org.json.JSONObject;
-import org.json.JSONTokener;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-class ItemsJSON {
+public class ItemsJSON {
 
-    public static void jsonReader(String item){
+    public static void jsonReader(String item) {
         String path = "./static/items.json";
 
         try {
-            FileReader fileReader = new FileReader(path);
-            JSONTokener jsonTokener = new JSONTokener(fileReader);
+            Gson gson = new Gson();
+            // Read JSON data from the file into a JsonObject
+            JsonObject jsonObject = gson.fromJson(new FileReader(path), JsonObject.class);
 
-            JSONObject jsonObject = new JSONObject(jsonTokener);
-
-            //This can remain the same
-            JSONObject locationObject = jsonObject.getJSONObject("location");
+            // This can remain the same
+            JsonObject locationObject = jsonObject.getAsJsonObject("location");
 
             // We will want to generate this dynamically based on actual player location
-//            PlayerLocation.getLocationName()
-            JSONObject commandObject = locationObject.getJSONObject("Command Center");
+            // PlayerLocation.getLocationName()
+            JsonObject commandObject = locationObject.getAsJsonObject("Command Center");
 
             // This might be able to remain static... depends on how we want to separate inter-actables
-            JSONObject itemsObject = commandObject.getJSONObject("items");
+            JsonObject itemsObject = commandObject.getAsJsonObject("items");
 
             // This will need to be dynamic based on whatever player has input
-
-            String items = itemsObject.getString(item);
+            String items = itemsObject.get(item).getAsString();
 
             System.out.println(items);
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
